ae2feb3063c3c20897fa410ad13ee3c5962690e1
Kotani <lyise771@gmail.com>
2023-06-24 09:24:15 +0900

    セッションスコープ追加85～87行目

--- teamdevelopment/src/main/java/servlet/LoginController.java
+++ teamdevelopment/src/main/java/servlet/LoginController.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpSession;
 
 import object.LoginAccount;
 import sql.MemberDAO;
+import validation.ValidateRegister;
 
 // URLパターン
 @WebServlet("/LoginController")
@@ -80,7 +82,7 @@ public class LoginController extends HttpServlet {
 			}
 
 			//セッションスコープに登録ユーザーを保存
-			LoginAccount loginAccount = new LoginAccount(id,pass);
+			LoginAccount loginAccount = new LoginAccount(id, pass);
 			HttpSession session = request.getSession();
 			session.setAttribute("loginAccount", loginAccount);
 
@@ -90,9 +92,30 @@ public class LoginController extends HttpServlet {
 
 			// 新規会員登録をして会員情報をDBに登録	
 		} else if (newRegister.equals("ok")) {
+			/****************************************
+			  
+			ここから下は修正していく
+			
+			*********************************************/
 			// Validationメソッドを実行して
-			// 条件と一致していたら
-
+			// エラーがなければDBに登録
+			//Validation機能
+			//テキストボックスのtextを保持したFormの作成
+			
+			ValidateRegister validateRegister = new ValidateRegister( id, address, pass);
+			request.setAttribute("validateRegister", validateRegister);
+			ArrayList<String> messageAll = new ArrayList<>(); 
+			messageAll.add(validateRegister.getMessageId());
+			messageAll.add(validateRegister.getMessageAddress());
+			messageAll.add(validateRegister.getMessagePass());
+			// メッセージがあればtrue
+			if(messageAll!= null) {
+				// フォワード(登録画面) 
+				// Validationメッセージがあれば新規会員登録画面に戻る
+				RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/registerForm.jsp");
+				dispatcher.forward(request, response);
+			}
+			
 			// 登録するユーザーの情報を設定
 			MemberDAO memDAO = new MemberDAO();
 			// DB接続して会員情報を登録(INSERT)
@@ -100,8 +123,7 @@ public class LoginController extends HttpServlet {
 			// フォワード(ログイン画面) 
 			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
 			dispatcher.forward(request, response);
-			
-			
+
 		}
 	}
 }
